edfacc360c66f75138351fb7f5a2c6bf5d5fe193
Oklmboy <sabandadanielrayann@gmail.com>
2024-09-10 12:51:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require("./all/module")
 
 global.owner = "https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04" 
-global.namabot = "𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐" //BOT NAME
-global.namaCreator = "*PRECIOUS AYOMIDE*" //CREATOR NAME
+global.namabot = "OKLMBOY" //BOT NAME
+global.namaCreator = "OKLMBOY" //CREATOR NAME
 global.autoJoin = false //DON'T CHANGE  / JANGAN GANTI
 global.antilink = false //DON'T CHANGE  / JANGAN GANTI
 global.versisc = '1.0.0' //DON'T CHANGE 
@@ -12,8 +12,8 @@ global.imageurl = 'https://telegra.ph/file/6b841282edd26fa590b6c.jpg' //GANTI PP
 global.isLink = 'https://whatsapp.com/channel/0029VaaUfPO8qIzztuf42D04' 
 global.thumb = fs.readFileSync("./thumb.png") ///DON'T CHANGE  
 global.audionya = fs.readFileSync("./all/sound.mp3") //DON'T CHANGE  
-global.packname = "𝐁𝐲 𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻𝟐 " 
-global.author = "*PRECIOUS AYOMIDE👑*" 
+global.packname = "BY OKLMBOY  " 
+global.author = "*OKLMBOY👑*" 
 global.jumlah = "5" ////DON'T CHANGE
 
 let file = require.resolve(__filename)
